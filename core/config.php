f3a75434564b1bd33e1a79edfebc75cc768925c2
Christopher Chance <christopherchance67@gmail.com>
2017-09-08 07:19:14 -0300

    Subida

---
+++ core/config.php
@@ -0,0 +1,19 @@
+<?php 
+
+	/* 
+	 * En este archivo se definiran la informacion de configuracion del API,
+	 * variables, constantes y funciones requeridas para el resto de los archivos
+	*/
+
+	// Direccion del servidor de Base de datos
+	define("DB_HOST", "localhost");
+
+	// Nombre de usuario de Base de datos
+	define("DB_USER", "root");
+
+	// Clave de usuario de Base de datos
+	define("DB_PASS", "bob123");
+
+	// Nombre de la tabla sobre la cual se trabajara
+	define("DB_NAME", "api");
+?>
